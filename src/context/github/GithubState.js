7ef09d4b12a2f36170cf963b6e46fd49b3c39093
jequex <onyejekwe.harddisk20@gmail.com>
2020-09-22 18:55:40 +0100

    good

--- src/context/github/GithubState.js
+++ src/context/github/GithubState.js
@@ -4,6 +4,17 @@ import GithubContext from './githubContext';
 import GithubReducer from './githubReducer';
 import {SEARCH_USERS, SET_LOADING, CLEAR_USERS, GET_USER} from '../types';
 
+let githubClientId;
+let githubClientSecret;
+
+if(process.env.NODE_ENV !== "production"){
+    githubClientId = process.env.USER_FINDER_APP_CLIENT_ID;
+    githubClientSecret = process.env.USER_FINDER_APP_CLIENT_SECRET;
+}else{
+    githubClientId = process.env.CLIENT_ID;
+    githubClientSecret = process.env.CLIENT_SECRET;
+}
+
 
 const GithubState = props => {
     const initialState = {
@@ -18,8 +29,8 @@ const GithubState = props => {
     const searchUsers = async (text) => {
         setLoading()
     
-        const res = await axios.get(`https://api.github.com/search/users?q=${text}&client_id=${process.env.USER_FINDER_APP_CLIENT_ID}
-        &client_secret=${process.env.USER_FINDER_APP_CLIENT_SECRET}`);
+        const res = await axios.get(`https://api.github.com/search/users?q=${text}&client_id=${githubClientId}
+        &client_secret=${githubClientSecret}`);
     
         dispatch({
             type: SEARCH_USERS,
@@ -32,8 +43,8 @@ const GithubState = props => {
     const getUser = async (username) => {
         setLoading()
 
-        const res = await axios.get(`https://api.github.com/users/${username}?client_id=${process.env.USER_FINDER_APP_CLIENT_ID}
-        &client_secret=${process.env.USER_FINDER_APP_CLIENT_SECRET}`);
+        const res = await axios.get(`https://api.github.com/users/${username}?client_id=${githubClientId}
+        &client_secret=${githubClientSecret}`);
 
         dispatch({
             type: GET_USER,
